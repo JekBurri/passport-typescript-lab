cb6eec771b9e7f0d632b29b5bcd65d59372f856f
Jack Boeri <53635108+JekBurri@users.noreply.github.com>
2023-02-11 21:18:11 -0800

    fixed types

--- middleware/checkAuth.ts
+++ middleware/checkAuth.ts
@@ -7,9 +7,6 @@ export const ensureAuthenticated = (req: Request, res: Response, next: NextFunct
   res.redirect("/auth/login");
 }
 
-/*
-FIX ME (types) 😭
-*/
 export const forwardAuthenticated = (req: Request, res: Response, next: NextFunction) => {
     if (!req.isAuthenticated()) {
       return next();
